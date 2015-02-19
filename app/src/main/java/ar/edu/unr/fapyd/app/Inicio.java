f7a1b5ffa9d729594e8e20d3e99440bb618f3d73
Esteban López Adriano <estebanlopeza@gmail.com>
2015-02-19 12:57:57 -0300

    Primera app.

--- app/src/main/java/ar/edu/unr/fapyd/app/Inicio.java
+++ app/src/main/java/ar/edu/unr/fapyd/app/Inicio.java
@@ -4,9 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class Inicio extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "ar.edu.unr.fapyd.app.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +39,14 @@ public class Inicio extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
